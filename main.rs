68330788edcdfb06206d6e5928388556a6f27a7a
fhh200000 <1041154549@qq.com>
2022-06-19 22:36:50 +0800

    Update: Chapter 2.

--- main.rs
+++ main.rs
@@ -1,3 +1,22 @@
+extern crate rand;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Hello world!");
+    println!("Input whatever you want:");
+    let rand = rand::thread_rng().gen_range(1,101);
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to input!");
+        let num:u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => continue
+        };
+        match num.cmp(&rand) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {println!("Same!");break;}
+        }
+    }
 }
